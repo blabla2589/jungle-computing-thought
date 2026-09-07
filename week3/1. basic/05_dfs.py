8b53c282028a397eea7bb900d197897b0989043d
blablab2589 <blabla2589@gmail.com>
2026-09-07 11:21:35 +0900

    solve basic 05

--- week3/1. basic/05_dfs.py
+++ week3/1. basic/05_dfs.py
@@ -43,15 +43,18 @@ def dfs(graph, start, visited=None):
         방문 순서 리스트
     """
     # TODO: visited가 None이면 초기화
-    pass
+    if visited is None:
+        visited = []
     
     # TODO: 현재 정점 방문
-    pass
+    if start not in visited:
+        visited.append(start)
     
     # TODO: 인접한 정점들에 대해 재귀
     ## 방문하지 않은 정점이면 재귀 호출
-    pass
-    
+    for next in graph[start]:
+        if next not in visited:
+            dfs(graph, next, visited=visited)
     return visited
 
 # 테스트 케이스
@@ -68,5 +71,4 @@ if __name__ == "__main__":
     result = dfs(graph, 0)
     print(f"시작 정점: 0")
     print(f"방문 순서: {result}")
-
 
